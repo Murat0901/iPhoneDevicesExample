6a3a7cce57b276efa0a658f8328a7010454aa1cb
Murat Menzilci <menzilcim>
2021-10-18 10:20:15 +0300

    Initial Commit

---
+++ iPhoneDevicesExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPhoneDevicesExample
+//
+//  Created by Murat Menzilci on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
